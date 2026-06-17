b855871ce78b85ea0aba6a97339952187230751c
Surya Khan <surya.khan@example.com>
2026-06-17 15:15:27 +0700

    chore: remove postgres adapter for sqlite

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,3 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
-
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
-
+const prisma = new PrismaClient();
 module.exports = prisma;
